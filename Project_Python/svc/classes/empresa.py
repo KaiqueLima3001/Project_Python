269fdc2c4c4f132349255749759712ff94a49d3f
Vitor Lamoya <131486302+VitorLamoya@users.noreply.github.com>
2024-10-31 23:59:13 -0300

    Delete Project_Python directory

--- Project_Python/svc/classes/empresa.py
+++
@@ -1,8 +0,0 @@
-class Empresa:
-    def __init__(self, nome, cnpj):
-        self.nome = nome
-        self.cnpj = cnpj
-    
-    @classmethod
-    def addEmpresa(cls,nome,cnpj):
-        return cls(nome,cnpj)
